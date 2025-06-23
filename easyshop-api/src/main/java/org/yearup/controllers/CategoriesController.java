1e96be544b62dca4ff592a231b0a10fa832a6cfa
Randyglobal <randyitoe2255@gmail.com>
2025-06-23 14:39:37 -0400

    Understanding Authentication and Implementing MysqlCategoryDao

--- easyshop-api/src/main/java/org/yearup/controllers/CategoriesController.java
+++ easyshop-api/src/main/java/org/yearup/controllers/CategoriesController.java
@@ -1,6 +1,8 @@
 package org.yearup.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 import org.yearup.data.CategoryDao;
@@ -23,8 +25,7 @@ public class CategoriesController
     private ProductDao productDao;
 
     // create an Autowired controller to inject the categoryDao and ProductDao
-
-
+    @Autowired
     public CategoriesController(CategoryDao categoryDao, ProductDao productDao) {
         this.categoryDao = categoryDao;
         this.productDao = productDao;
@@ -44,15 +45,28 @@ public class CategoriesController
     }
 
     // add the appropriate annotation for a get action
+    @GetMapping("{id}")
+    @PreAuthorize("permitAll()")
     public Category getById(@PathVariable int id)
     {
         // get the category by id
-        return null;
+        try {
+            Category category = categoryDao.getById(id);
+            if (category == null){
+                throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Category not found with ID: " + id);
+            }
+            return category;
+
+        }catch (Exception e){
+            System.err.println("Error fetching categories: " + e.getMessage());
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Oops... our bad. Could not retrieve categories.");
+        }
     }
 
     // the url to return all products in category 1 would look like this
     // https://localhost:8080/categories/1/products
     @GetMapping("{categoryId}/products")
+    @PreAuthorize("permitAll()")
     public List<Product> getProductsById(@PathVariable int categoryId)
     {
         // get a list of product by categoryId
--- easyshop-api/src/main/java/org/yearup/data/mysql/MySqlCategoryDao.java
+++ easyshop-api/src/main/java/org/yearup/data/mysql/MySqlCategoryDao.java
@@ -5,10 +5,7 @@ import org.yearup.data.CategoryDao;
 import org.yearup.models.Category;
 
 import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -42,9 +39,22 @@ public class MySqlCategoryDao extends MySqlDaoBase implements CategoryDao
     }
 
     @Override
-    public Category getById(int categoryId)
-    {
+    public Category getById(int categoryId){
         // get category by id
+        String sql = "SELECT category_id, name, description From categories WHERE category_id = ?";
+        try(Connection connection = getConnection()) {
+            PreparedStatement statement = connection.prepareStatement(sql);
+            statement.setInt(1, categoryId);
+
+            ResultSet row = statement.executeQuery();
+
+            if (row.next()){
+                return mapRow(row);
+            }
+        }catch (SQLException e){
+            System.err.println("Error in getById for categoryId " + categoryId + ": " + e.getMessage());
+            throw new RuntimeException(e);
+        }
         return null;
     }
 
@@ -52,19 +62,65 @@ public class MySqlCategoryDao extends MySqlDaoBase implements CategoryDao
     public Category create(Category category)
     {
         // create a new category
-        return null;
+        String sql = "INSERT INTO categories (name, description) VALUES (?, ?)";
+        try(Connection connection = getConnection()) {
+            PreparedStatement statement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            statement.setString(1, category.getName());
+            statement.setString(2, category.getDescription());
+
+            int rowAffected = statement.executeUpdate();
+            if (rowAffected > 0){
+                ResultSet row = statement.getGeneratedKeys();
+                if (row.next()){
+                    int newCategoryId = row.getInt(1);
+                    return getById(newCategoryId);
+                }
+            }
+            throw new RuntimeException("Category creation failed, no ID obtained.");
+        }catch (SQLException e){
+            System.err.println("Error in create category: " + e.getMessage());
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public void update(int categoryId, Category category)
     {
         // update category
+        String sql = "UPDATE categories SET name = ?, description = ? WHERE categories_id = ?";
+        try(Connection connection = getConnection()) {
+            PreparedStatement statement = connection.prepareStatement(sql);
+            statement.setString(1, category.getName());
+            statement.setString(2, category.getDescription());
+            statement.setInt(3, categoryId); // Using the ID from the path
+
+            int rowAffected = statement.executeUpdate();
+            if (rowAffected == 0){
+                System.out.println("No category of type " + categoryId + " found");
+            }
+        }catch (SQLException e){
+            System.err.println("Error in update categoryId " + categoryId + ": " + e.getMessage());
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public void delete(int categoryId)
     {
         // delete category
+        String sql = "DELETE FROM categories WHERE categories_id = ?";
+        try(Connection connection = getConnection()) {
+            PreparedStatement statement = connection.prepareStatement(sql);
+            statement.setInt(1, categoryId); // Using the ID from the path
+
+            int rowAffected = statement.executeUpdate();
+            if (rowAffected == 0){
+                System.out.println("No category of type " + categoryId + " found");
+            }
+        }catch (SQLException e){
+            System.err.println("Error in update categoryId " + categoryId + ": " + e.getMessage());
+            throw new RuntimeException(e);
+        }
     }
 
     private Category mapRow(ResultSet row) throws SQLException
